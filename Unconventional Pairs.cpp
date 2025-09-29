2fcc4db3060afac627e5cac5f1fc23ec6710069f
Saleh-Nairat <nuiratsaleh@gmail.com>
2025-09-29 18:00:12 +0300

    Codeforces: solution for Unconventional Pairs problem

---
+++ Unconventional Pairs.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en '\n'
+#define ll long long
+#define tc int t;cin>>t;while(t--)
+#define yes cout << "YES" << en;
+#define no cout << "NO" << en;
+#define v vector<int>
+#define vl vector<ll>
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+
+
+    tc
+    {
+        int n,result=INT_MIN;
+        cin >> n;
+
+        v nums(n);
+
+        for(auto &e : nums)
+            cin >> e;
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n-1;i+=2)
+        {
+            int temp=abs(nums[i]-nums[i+1]);
+            result=max(result,temp);
+        }
+
+        cout << result << en;
+
+    }
+    return  0;
+}
