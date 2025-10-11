d767747c4a3dcbb8d8cf02467a7bc1a914b38d67
Saleh-Nairat <nuiratsaleh@gmail.com>
2025-10-11 11:36:56 +0300

    Codeforces: solution for Review Site problem

---
+++ Review Site.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en '\n'
+#define ll long long
+#define tc int t;cin>>t;while(t--)
+#define yes cout << "YES" << en;
+#define no cout << "NO" << en;
+#define vi vector<int>
+#define vl vector<ll>
+#define vd vector<double>
+#define vc vector<char>
+bool isVowel(char c)
+{
+    return c=='a' || c=='u' || c=='i' || c=='e' || c=='o';
+}
+
+bool isPalindrome(string s)
+{
+    string temp=s;
+
+    reverse(temp.begin(),temp.end());
+
+    return temp==s;
+}
+
+int binary_search(vector<int> nums,int target)
+{
+    int low=0,high=nums.size()-1;
+
+    while(low<=high)
+    {
+
+        int mid=low + (high - low) / 2;
+
+        if(nums[mid]==target) {return mid;}
+
+        else if(nums[mid]>target) {high=mid-1;}
+
+        else {low=mid+1;}
+
+    }
+
+    return -1;
+}
+int SumOfDigit(int n)
+{
+    int result=0;
+
+    while(n) {result+=n%10; n/=10;}
+
+    return result;
+}
+
+bool isPrime(int n)
+{
+    if(n==2) {return true;}
+    if(n%2==0 || n<=1) {return false;}
+
+    for(int i=3;i*i<=n;i+=2)
+    {
+        if(n%i==0) {return false;}
+    }
+
+    return true;
+}
+
+
+
+
+
+void Saleh()
+{
+    int n; cin >> n;
+    int result=0;
+
+    vi reviewers(n);
+
+    for(auto &e : reviewers) {cin >> e;}
+
+    for(auto &e : reviewers)
+    {
+        if(e==1 || e==3) {result++;}
+    }
+
+    cout << result << en;
+}
+
+
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+
+
+    tc
+    {
+        Saleh();
+    }
+
+
+    return  0;
+}
